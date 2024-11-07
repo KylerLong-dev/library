41ae2457a912d8dd7f846d83ed205b13bfa79e7d
Kyler Long <kylerlong24@gmail.com>
2024-11-07 09:48:11 -0500

    write logic for object constructor function

--- script.js
+++ script.js
@@ -1,9 +1,25 @@
+//addbook function needs to take in args that will create a book from those args and store in array
+//the books added to the array will be ojects (include title, author, pages, and if complete or unread)
+//write a function that loops through the array and displays each book on its own card (DOM function)
+
 const myLibrary = [];
 
-function Book() {
-  // the constructor...
+function Book(title, author, pages, read) {
+  this.title = title; 
+  this.author = author; 
+  this.pages = pages; 
+  this.read = read; 
+  this.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages}, ${this.read}`
+  }
 }
+
+const bookOne = new Book("The Hobbit", "J.R.R. Tolkien", "295 pages", "Not Read Yet");
 
 function addBookToLibrary() {
   // do stuff here
+}
+
+function displayBooks () {
+
 }
