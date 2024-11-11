0f8b1b4a262ef052efe2111c8713503d53f51f8d
Kyler Long <kylerlong24@gmail.com>
2024-11-11 18:11:04 -0500

    Create logic to accept user date from form input and use values to run through addBook function as arguments

--- script.js
+++ script.js
@@ -16,24 +16,59 @@ function addBookToLibrary(title, author, pages, read) {
 
 function displayBook() {
   const bookContainer = document.querySelector(".grid-container");
+
+  bookContainer.innerHTML = "";
+
   myLibrary.forEach((book) => {
     const bookCard = document.createElement("div");
     bookCard.classList.add("grid-container_book");
+
+    let readStatus = "";
+    if (book.read) {
+      readStatus = "Read Status: Complete!";
+    }
+    else {
+      readStatus = "Read Status: Not Read";
+    }
+
     bookCard.innerHTML = `
       <h3>${book.title}</h3>
       <p>${book.author}</p>
-      <p>${book.pages}</p>
-      <p>${book.read}</p>
+      <p>${book.pages} pages</p>
+      <p>${readStatus}</p>
       `;
     bookContainer.appendChild(bookCard);
   })
 }
 
-addBookToLibrary("The Hobbit", "J.R.R Tolkien", "535 pages", "Read")
-displayBook();
+const addBookBtn = document.querySelector(".new-book_btn"); 
+const sideBar = document.querySelector(".sidebar");
 
-function userInput() {
+addBookBtn.addEventListener("click", () => {
+  if (!sideBar.classList.contains("show")) {
+    sideBar.classList.add("show");
+  }
+})
+
+const form = document.querySelector(".sidebar-form")
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const author = document.getElementById("author").value;
+  const pages = document.getElementById("pages").value;
+  const read = document.getElementById("finished").checked; 
   
-}
+  addBookToLibrary(title, author, pages, read);
+  displayBook();
 
+  form.reset();
+})
+
+const sidebarClose = document.querySelector(".sidebar-close")
+
+sidebarClose.addEventListener("click", () => {
+  sideBar.classList.remove("show");
+})
 
